5747647b1876c047bf34868eb544ab8bc2dad655
remi70 <r.fournier70@gmail.com>
2020-02-03 14:42:08 +0100

    fix on heros model

--- src/models/heros.ts
+++ src/models/heros.ts
@@ -2,11 +2,12 @@ export class Heros {
     nom:string="";
     classe:string="";
     arme:string="";
-    hp:number=0;
+    hp:number;
 
 constructor(nom:string, classe:string,arme:string,hp:number){
     this.nom = nom;
     this.classe = classe;
     this.arme = arme;
+    this.hp = hp;
 }
 }
