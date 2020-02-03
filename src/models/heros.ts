ca6f4b3ff0a7dd8647df4178561ce172da12eefc
remi70 <r.fournier70@gmail.com>
2020-02-03 14:39:41 +0100

    add hp to heros model

--- src/models/heros.ts
+++ src/models/heros.ts
@@ -2,8 +2,9 @@ export class Heros {
     nom:string="";
     classe:string="";
     arme:string="";
+    hp:number=0;
 
-constructor(nom:string, classe:string,arme:string){
+constructor(nom:string, classe:string,arme:string,hp:number){
     this.nom = nom;
     this.classe = classe;
     this.arme = arme;
