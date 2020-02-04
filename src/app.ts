e42279c007a597374c9b55ff74f3dbdcdee8af2b
remi70 <r.fournier70@gmail.com>
2020-02-04 17:02:32 +0100

    WIP connect back to front

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import { setRoutes } from "./app.routing";
+import cors from "cors";
 
 
 export const app: Application = express ();
@@ -8,6 +9,7 @@ const port:string="3000";
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 app.listen(port,()=>{
     console.log(`Server is running on port:${port}`);
 })
